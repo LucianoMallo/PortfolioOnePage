39a9548847842adf7803a24f361794a1fc243d49
Luciano Mallo <malloluciano@gmail.com>
2023-07-18 18:03:37 +0200

    .

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './projects.css'
 import ProjectCard from './projectsCard/projectCard'
-import mineSweeper from '../../assets/minesweeperPlaceholder.webp'
-import streamberry from '../../assets/streamberryPlaceholder.webp'
-import ecommerce from '../../assets/ecommercePlaceholder.webp'
-import library from '../../assets/libraryPlaceholder.webp'
+import mineSweeper from '../../assets/minesweeperplaceholder.webp'
+import streamberry from '../../assets/streamberryplaceholder.webp'
+import ecommerce from '../../assets/ecommerceplaceholder.webp'
+import library from '../../assets/libraryplaceholder.webp'
 
 function Projects () {
   return (
